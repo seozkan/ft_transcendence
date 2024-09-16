129199576cb065678005788b63925c068acc37c5
Seckin <ozkan003@gmail.com>
2024-09-16 13:38:13 +0300

    Add superuser creation script to exec.sh

--- core/exec.sh
+++ core/exec.sh
@@ -18,5 +18,15 @@ done
 
 python manage.py makemigrations
 python manage.py migrate
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+
+username = "${DJANGO_ADMIN_USER}"
+email = "${DJANGO_ADMIN_MAIL}"
+password = "${DJANGO_ADMIN_PASSWORD}"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
 
 exec "$@"
