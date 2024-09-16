0a990e5c14ee66316d9a398cb280bc7dda9f34fc
Seckin <ozkan003@gmail.com>
2024-09-16 12:14:58 +0300

    Add Bootstrap support and update static file configuration

--- core/core/settings.py
+++ core/core/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'www'],
+        'DIRS': [BASE_DIR / "www"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'www']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
