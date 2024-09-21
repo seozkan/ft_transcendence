ad71a65d926a64bbad26242ede13e3b16ae6f849
Seçkin Ersin Özkan <119052402+seozkan@users.noreply.github.com>
2024-09-21 17:46:39 +0300

    Update exec.sh

--- core/exec.sh
+++ core/exec.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-MAX_RETRIES=10
-RETRY_COUNT=0
-RETRY_INTERVAL=2
-
 echo "Exec.sh is running..."
 
+python manage.py makemigrations
+
 until python manage.py migrate --noinput; do
-  if [ $RETRY_COUNT -ge $MAX_RETRIES ]; then
-    echo "Database is not ready after $MAX_RETRIES attempts. Exiting."
-    exit 1
-  fi
   echo "Waiting for database to be ready..."
-  sleep $RETRY_INTERVAL
-  RETRY_COUNT=$((RETRY_COUNT + 1))
+  sleep 3
 done
 
-python manage.py makemigrations
-python manage.py migrate
 python manage.py shell << EOF
 from django.contrib.auth.models import User
 
@@ -28,6 +19,7 @@ password = "${DJANGO_ADMIN_PASSWORD}"
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
 EOF
+
 python manage.py collectstatic --noinput
 
 exec "$@"
